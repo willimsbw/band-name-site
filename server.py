37df7068ffcccc005709ca8a1ff69c64758d8af0
Bryan <willimsbw@gmail.com>
2018-08-30 12:55:43 -0400

    Created empty DB. Got basic functionality working

--- server.py
+++ server.py
@@ -1,7 +1,5 @@
-from database_setup import Base, Restaurant, MenuItem, User
+from database_setup import Base, Words
 from flask import Flask, redirect, render_template, request, url_for
-from oauth2client.client import flow_from_clientsecrets
-from oauth2client.client import FlowExchangeError
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import random, string
@@ -12,7 +10,7 @@ DBSession = sessionmaker(bind = engine)
 session = DBSession()
 app = Flask(__name__)
 
-@app.route('/' methods=["POST", "GET"])
+@app.route('/', methods=["POST", "GET"])
 def generator():
     if request.method == "GET":
         # Show button with band name over it
@@ -22,8 +20,8 @@ def generator():
         return render_template('homepage.html', first=first, second=second)
     if request.method == "POST":
         # Get random values and redirect with them as part of the query URL
-        # first =
-        # second =
+        first = "first"
+        second = "second"
         return redirect(url_for("generator") + "?first=" + first + "&second=" + second, code=303)
 
 if __name__ == '__main__':

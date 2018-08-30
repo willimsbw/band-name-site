e4a05e50153916d81de72cccf81e9eb3edbe5c91
Bryan <willimsbw@gmail.com>
2018-08-30 14:08:35 -0400

    Added 10 words to populate database

--- fill_db.py
+++ fill_db.py
@@ -21,9 +21,40 @@ session = DBSession()
 
 
 #Adding words! for UrbanBurger
-word1 = Words(word = "fox")
-
+word1 = Words(word = "Fox")
 session.add(word1)
+session.commit()
+
+word2 = Words(word = "Gorge")
+session.add(word2)
+session.commit()
+
+word3 = Words(word = "Canyon")
+session.add(word3)
+session.commit()
+
+word4 = Words(word = "Platypus")
+session.add(word4)
+session.commit()
+
+word5 = Words(word = "Deer")
+session.add(word5)
+session.commit()
+
+word6 = Words(word = "Mountain")
+session.add(word6)
+session.commit()
+
+word7 = Words(word = "Lake")
+session.add(word7)
+session.commit()
+
+word8 = Words(word = "Dog")
+session.add(word8)
+session.commit()
+
+word9 = Words(word = "Man")
+session.add(word9)
 session.commit()
 
 print("added words!")

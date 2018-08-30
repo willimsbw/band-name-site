9cdd279a5ca794d711c977526057302e741c64d0
Bryan <willimsbw@gmail.com>
2018-08-30 13:11:20 -0400

    Fixed a missing import for Base

--- fill_db.py
+++ fill_db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from database_setup import Words
+from database_setup import Words, Base
 
 engine = create_engine('sqlite:///words.db')
 # Bind the engine to the metadata of the Base class so that the

4d501366fbd56b76fe330f00f06a8cb1e9b9baab
Bryan Williams <Bryan Williams>
2021-10-29 23:30:24 -0400

    Trying out new styling on navabar

--- band-name-site/src/App.js
+++ band-name-site/src/App.js
@@ -6,7 +6,9 @@ import NavBar from './NavBar'
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { showManage: false }
+    this.state = { showManage: false };
+    this.toggleHomePage = this.toggleHomePage.bind(this);
+    this.toggleManagePage = this.toggleManagePage.bind(this);
   }
 
   toggleManagePage(e) {
@@ -21,27 +23,17 @@ class App extends React.Component {
     let body;
     if(this.state.showManage) {
       body = (
-        <div>
-          <h1>My State: {JSON.stringify(this.state)}</h1>
-          <h2>management page</h2>
-        </div>
-      )
+        <body className="App-body">
+          <div>
+            <h1>My State: {JSON.stringify(this.state)}</h1>
+            <h2>management page</h2>
+          </div>
+        </body>
+      );
     } else {
       body = (
         <body className="App-body">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <h1> Testing live rebuild </h1>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
           <h1>My State: {JSON.stringify(this.state)}</h1>
         </body>
       );
@@ -49,7 +41,7 @@ class App extends React.Component {
 
     return (
       <div className="App">
-        <header><NavBar onHomeClick={this.toggleHomePage.bind(this)} onManageClick={this.toggleManagePage.bind(this)} /></header>
+        <header><NavBar onHomeClick={this.toggleHomePage} onManageClick={this.toggleManagePage} /></header>
         {body}
       </div>
     );
--- band-name-site/src/NavBar.js
+++ band-name-site/src/NavBar.js
@@ -1,25 +1,26 @@
 import './NavBar.css';
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from "prop-types";
 
-class NavBar extends Component {
+class NavBar extends React.Component {
     render() {
-        return (
-          <div class="header-row">
-            <div onClick={this.props.onHomeClick} className="header-entry">
-              Home
-            </div>
-            <div onClick={this.props.onManageClick} className="header-entry">
-              Manage
-            </div>
+      return (
+        <div class="header-row">
+          <div onClick={this.props.onHomeClick} className={this.props.showManage ? "header-entry" : "header-entry active"}>
+            Home
           </div>
-        );
+          <div onClick={this.props.onManageClick} className={this.props.showManage ? "header-entry active" : "header-entry"}>
+            Manage
+          </div>
+        </div>
+      );
     }
 }
 
 NavBar.propTypes = {
   onHomeClick: PropTypes.func.isRequired,
-  onManageClick: PropTypes.func.isRequired
+  onManageClick: PropTypes.func.isRequired,
+  showManage: PropTypes.bool.isRequired
 }
 
 export default NavBar;
